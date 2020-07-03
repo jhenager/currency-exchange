d49c5ec4d545d15ece52fd2a784c75d225bd469a
James Henager <jameschenager@gmail.com>
2020-07-03 13:56:09 -0700

    add reworked submit feature to convert japanese and canadian currencies

--- src/main.js
+++ src/main.js
@@ -7,22 +7,41 @@ import { Exchange } from './../src/exchange.js';
 
 
 $(document).ready(function () {
-  $('#submit-btn').click(function () {
+  $('#submit-canada').click(function () {
     const usAmount = $('#usAmount').val();
-    // const foreignCurrency = $('#foreignCurrency').val();
-
+    
     (async () => {
       let exchange = new Exchange();
       const response = await exchange.getMoney();
       getElements(response);
     })();
 
+    
     function getElements(response) {
       if (response) {
         $('#output').text((usAmount*`${response.conversion_rates.CAD}`));
       } else {
         $('#output').text(`The API doesn't work`);
       }
-    }
+    }   
+  });
+
+  $('#submit-japan').click(function () {
+    const usAmount = $('#usAmount').val();
+    
+    (async () => {
+      let exchange = new Exchange();
+      const response = await exchange.getMoney();
+      getElements(response);
+    })();
+
+    
+    function getElements(response) {
+      if (response) {
+        $('#output').text((usAmount*`${response.conversion_rates.JPY}`));
+      } else {
+        $('#output').text(`The API doesn't work`);
+      }
+    }   
   });
 });
